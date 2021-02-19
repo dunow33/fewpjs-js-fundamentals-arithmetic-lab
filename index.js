1dfd121d92af68be4d991b689b67e112a5a22b84
dunow <dunow33@hotmail.com>
2021-02-19 01:58:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()) + 1;
+
+const num3 = 39;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(20, 1, 3, 6);
